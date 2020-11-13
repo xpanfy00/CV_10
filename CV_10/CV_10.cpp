b59f013331690d6c44bd595c09144e29db413b99
xpanfy00 <74433956+xpanfy00@users.noreply.github.com>
2020-11-13 23:43:18 +0100

    Добавьте файлы проекта.

---
+++ CV_10/CV_10.cpp
@@ -0,0 +1,36 @@
+﻿#include "pch.h"
+#include "solution.h"
+#include <iostream>
+
+int main()
+{
+	char str[512];
+	int a = 0, n = 0, w = 0;
+	FILE* open;
+	errno_t errorCode = fopen_s(&open, "vstup.txt", "r");
+	if (open == NULL) {
+		perror("Chyba pri otevirani souboru");
+		return(-1);
+	}
+	while (!feof(open))
+	{
+		if (fscanf_s(open, "%s", str, 100) > 0)
+		{
+			a = a + alpha(str);
+			n = n + numb(str);
+			w = w + numb_of_word(str);
+		}
+
+		printf("pocet pismen %d , pocet cisel %d , pocet slov %d\n", a, n, w);
+
+		FILE* end;
+		errno_t errorCode1 = fopen_s(&end, "vysledky.txt", "w");
+		if (end == NULL) {
+			perror("Chyba pri otevirani souboru");
+			return(-1);
+		}
+		fprintf(end, "pocet pismen %d , pocet cisel %d , pocet slov %d\n", a, n, w);
+		fclose(end);
+	}
+
+}
---
+++ CV_10/solution.cpp
@@ -0,0 +1,67 @@
+#include "pch.h"
+#include "solution.h"
+#include <iostream>
+#include <ctype.h>
+#include<string.h>
+int alpha(char str[])
+{
+	int count = 0;
+	int l = strlen(str);
+	for (int i = 0; i < l; i++)
+	{
+		if (isalpha(str[i]))
+		{
+			count++;
+		}
+
+	}
+	return count;
+}
+
+int numb(char str[])
+{
+	int count = 0;
+	int l = strlen(str);
+	for (int i = 0; i < l; i++)
+	{
+		if (isdigit(str[i]))
+		{
+			count++;
+		}
+
+	}
+	return count;
+}
+
+int numb_of_word(char str[])
+{
+	int count = 0;
+	int l = strlen(str);
+		for (int i = 0; i < l; i++)
+		{
+			if (isalpha(str[i]))
+			{
+			
+				if (i == (l - 1))
+				{
+					count++;
+				}
+			}
+			else if (isspace(str[i]) && isalpha(str[i-1]))
+			{
+				count++;
+			}
+			else if (ispunct(str[i]) && isalpha(str[i-1]))
+			{
+				count++;
+			}
+
+			else
+			{
+				count = 0;
+			}
+
+		}
+	return count;
+}
+
